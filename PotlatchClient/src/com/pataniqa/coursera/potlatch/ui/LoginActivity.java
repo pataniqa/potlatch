fd82e73bc28f5b3538c070ed9e4329873377367e
Mark H. Butler <markhenrybutler@gmail.com>
2014-09-20 10:42:23 -0700

    Fixing error

--- PotlatchClient/src/com/pataniqa/coursera/potlatch/ui/LoginActivity.java
+++ PotlatchClient/src/com/pataniqa/coursera/potlatch/ui/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.pataniqa.coursera.potlatch.ui;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 
@@ -9,26 +10,29 @@ import com.pataniqa.coursera.potlatch.R;
 /**
  * The activity that allows the user to provide login information.
  */
-public class LoginActivity extends GiftActivityBase{
+public class LoginActivity extends GiftActivityBase {
 
-	private static final String LOG_TAG = LoginActivity.class.getCanonicalName();
-	
-	// The edit texts used
-	private EditText loginId;
-	private EditText password;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		// Setup the UI
+    private static final String LOG_TAG = LoginActivity.class.getCanonicalName();
+
+    // The edit texts used
+    private EditText loginId;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Setup the UI
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getActionBar().hide();
-		setContentView(R.layout.login_activity);
-		
-		//Find the edit texts
-		loginId = (EditText) findViewById(R.id.username);
-		password = (EditText) findViewById(R.id.password);
-	}
+        setContentView(R.layout.login_activity);
 
+        // Find the edit texts
+        loginId = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+    }
+
+    public void loginClicked(View v) {
+        openListGiftActivity();
+    }
 }
