85fede2971e28f5ae72a5f7250ff4a0d07009415
Mark H. Butler <markhenrybutler@gmail.com>
2014-10-14 17:26:36 -0700

    Fixing Gradle build

--- PotlatchCommon/src/main/java/com/pataniqa/coursera/potlatch/store/GiftResults.java
+++ PotlatchCommon/src/main/java/com/pataniqa/coursera/potlatch/store/GiftResults.java
@@ -1,17 +1,13 @@
 package com.pataniqa.coursera.potlatch.store;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import rx.Observable;
 import rx.functions.Func1;
 
 import com.pataniqa.coursera.potlatch.model.GiftResult;
 
-
 public class GiftResults {
-    
-    private static final String LOG_TAG = GiftResults.class.getCanonicalName();
     
     public static Observable<ArrayList<GiftResult>> hideFlaggedContent(Observable<ArrayList<GiftResult>> results,
             final boolean hide) {
